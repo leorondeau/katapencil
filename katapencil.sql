3abb4fa0931b57854add1ffffaabffc3ada798e5
Leo Rondeau <christopherleorondeau@gmail.com>
2021-11-29 10:25:39 -0600

    created an insert entries for pencil and table

---
+++ katapencil.sql
@@ -0,0 +1,17 @@
+
+
+
+-- CREATE TABLE 'Pencil' (
+--     'id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+--     'point_durability' INTEGER NOT NULL,
+--     'length' INTEGER NOT NULL,
+--     'eraser_durability' INTEGER NOT NULL
+-- );
+
+-- CREATE TABLE 'Journal' (
+--     'id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+--     'entry' TEXT NOT NULL
+-- );
+
+-- INSERT INTO 'Pencil' VALUES (null, 40000, 10, 20000);
+-- INSERT INTO 'Journal' VALUES (null, 'She sells sea shells');
